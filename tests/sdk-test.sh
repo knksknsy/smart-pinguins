8fc538b0a5b2117a8cfdfa91c6a29c4de38eb0a5
Keskinsoy Kaan <kk148@hdm-stuttgart.de>
2019-12-15 11:32:49 +0100

    Update sdk-test.sh

--- tests/sdk-test.sh
+++ tests/sdk-test.sh
@@ -2,7 +2,7 @@
 
 if hash git \
     && hash pip3 \
-    && hash arm-none-eabi-gcc \
+    && hash /gcc-arm-none-eabi-4_9-2015q3/bin/arm-none-eabi-gcc \
     && hash nrfjprog \
     && hash JLinkExe \
     && [ -d "$GNUARMEMB_TOOLCHAIN_PATH" ]; then
